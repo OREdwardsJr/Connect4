b27b3600e913df8ad19643e14b2276405ffcce2c
Orlando <oredwardsjr@gmail.com>
2022-09-25 00:18:40 -0700

    Added comment starting at line 20

--- test/com/connect4/comm/CommunicatorTest.java
+++ test/com/connect4/comm/CommunicatorTest.java
@@ -17,8 +17,18 @@ class CommunicatorTest {
         for (int i = 1; i < 3; i++){
             String name =prompter.prompt("Please enter name: ");
             int id= i;
-            player.setName(name);
-            player.setID(id);
+            /*
+             * From Orlando - I haven't tested this class. However, I'm guessing that
+             * the for loop is printing out the same name below (line 34)?
+             * If yes, it may be that everytime you call setName and setId
+             * you are changing it on the same object. You're storing the same object
+             * in the array 3 times. So, when you change the name and ID on one then
+             * you change it on all. I'd recommend that you uncomment line 29
+             * and comment out lines 30 and 31. See if that works.
+             */
+            // player = New Player(name, id)
+            player.setName(name); // comment out
+            player.setID(id);     // comment out
             players.add(player);
         }
         for (Player item: players){
