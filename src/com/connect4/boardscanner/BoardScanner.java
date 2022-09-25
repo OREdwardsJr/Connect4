8e3d2ed74d5b4a9c0d3e2c6b8a1fe9e881df6ef8
Orlando <oredwardsjr@gmail.com>
2022-09-24 21:52:17 -0700

    fixed checkRow in BoardScanner

--- src/com/connect4/boardscanner/BoardScanner.java
+++ src/com/connect4/boardscanner/BoardScanner.java
@@ -35,15 +35,17 @@ public class BoardScanner {
         if (++count == 4) return true;
 
         switch (option) { // there are at most 7 directions to check for winning moves
-            case 0: // travel East
+            case 0: // travel Northeast
+                return checkRow(option, count, player, board, --r, ++c);
+            case 1: // travel East
                 return checkRow(option, count, player, board, r, ++c);
-            case 1: // travel Southeast
+            case 2: // travel Southeast
                 return checkRow(option, count, player, board, ++r, ++c);
-            case 2: // travel South
+            case 3: // travel South
                 return checkRow(option, count, player, board, ++r, c);
-            case 3: // travel Southwest
+            case 4: // travel Southwest
                 return checkRow(option, count, player, board, ++r, --c);
-            case 4: // travel West
+            case 5: // travel West
                 return checkRow(option, count, player, board, r, --c);
             default: // travel Northwest
                 return checkRow(option, count, player, board, --r, --c);
