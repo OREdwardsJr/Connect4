34355b12282addb6321b0a53e84b8449794f073d
Orlando <oredwardsjr@gmail.com>
2022-09-25 13:49:25 -0700

    altered logic mostly in checkRow

--- src/com/connect4/CompLogic.java
+++ src/com/connect4/CompLogic.java
@@ -19,49 +19,39 @@ class CompLogic {
         return choice;
     }
 
-    private static List<Integer> swapNums(int arrSize) {
-        List<Integer> arr = new ArrayList<>(arrSize);
-
-        for (int i = 0; i < arrSize; i++) arr.set(i, i);
-
-        Collections.shuffle(arr);
-
-        return arr;
-    }
-
-    //TODO describe how this below method works
     /*
      * If the difficulty is set to medium then the computer searches for a move that would cause them to win,
      * or a move that can prevent a loss, or some move that matches the most connected cpu pieces
-     * Methods are listed in descending order.
+     * Methods are listed in descending dependency order.
      *  - medium() calls findBestOption() to return the highest value move
-     *  - findBestOption calls checkRow()
+     *  - findBestOption calls checkRow() on each row to capture the row's highest value
      *  - checkRow() returns the largest number of connected pieces
      */
-    private static int medium(int[][] board, int id) {
+    public static int medium(int[][] board, int id) {
         // index 0 is the position - index 1 is the count of connected pieces
         int[] cpuBestOption = new int[2];
         int[] opponentBestOption = new int[2];
 
-        opponentBestOption = findBestOption(board, id, false);
         cpuBestOption = findBestOption(board, id, true);
+        opponentBestOption = findBestOption(board, id, false);
 
-        if (cpuBestOption[1] == 4) return cpuBestOption[0];
+        if (cpuBestOption[1] == 4) return cpuBestOption[0]; // win game
 
-        if (opponentBestOption[1] == 4) return opponentBestOption[0];
+        if (opponentBestOption[1] == 4) return opponentBestOption[0]; // try to prevent winning move from opponent
 
-        return cpuBestOption[0];
+        return cpuBestOption[0]; // return option with the longest connection of cpu pieces
     }
 
     private static int[] findBestOption(int[][] board, int id, boolean useId) {
         int currentValue = 0;
         int[] bestOption = new int[2];
+        int r = 0; int c = 0; int count = 0;
 
-        for (int i = 0; i < board[0].length; i++) {
-            currentValue = checkRow(board, i, id, 0, 0, 0, useId);
+        for (int option = 0; option < board[0].length; option++) {
+            currentValue = checkRow(board, option, id, r, c, count, useId);
 
             if (currentValue > bestOption[1]) {
-                bestOption[0] = i;
+                bestOption[0] = option;
                 bestOption[1] = currentValue;
                 if (bestOption[1] == 4) break;
             }
@@ -70,17 +60,15 @@ class CompLogic {
     }
 
     private static int checkRow(int[][] board, int option, int id, int r, int c, int count, boolean useId) {
-        int result = 0;
+        if ((r < 0 || r >= board.length) || (c < 0 || c >= board[0].length)) return count; // if out of bounds
 
-        // TODO Need to verify that an if statement will not check subsequent conditions if condition has been found
-        //  if it continues to check then move "(++count == 4) beyond scope as count only be iterated in this scope
-        //  if none of the previous conditions are true while ++count == 4 is true.
-        if ((r < 0 || r >= board.length) ||
-                (c < 0 || c >= board[0].length) ||
-                (useId && id != board[r][c]) ||
-                (!useId && board[r][c] == id) ||
-                (++count == 4))
-            return count;
+        if (board[r][c] != 0) {
+            if ((useId && id != board[r][c]) || (!useId && board[r][c] == id)) {  // if board holds opponent's piece
+                return count;
+            }
+        }
+
+        if (++count == 4 || board[r][c] == 0) return count; // if winning option is present or slot is empty
 
         switch (option) { // there are at most 7 directions to check for winning moves
             case 0: // travel Northeast
@@ -98,5 +86,15 @@ class CompLogic {
             default: // travel Northwest
                 return checkRow(board, option, id, --r, --c, count, useId);
         }
+    }
+
+    private static List<Integer> swapNums(int arrSize) {
+        List<Integer> arr = new ArrayList<>(arrSize);
+
+        for (int i = 0; i < arrSize; i++) arr.set(i, i);
+
+        Collections.shuffle(arr);
+
+        return arr;
     }
 }
