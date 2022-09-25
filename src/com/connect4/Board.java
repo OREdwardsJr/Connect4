b941af3a0aef8ca505025e64d38bb46681569bc7
Orlando <oredwardsjr@gmail.com>
2022-09-24 23:25:19 -0700

    Changed var name indices to rowTracker to better explain purpose

--- src/com/connect4/Board.java
+++ src/com/connect4/Board.java
@@ -2,8 +2,6 @@ package com.connect4;
 
 import com.connect4.boardscanner.BoardScanner;
 
-import java.util.Scanner;
-
 public class Board {
     // Properties
     private static final Board BOARD = new Board();
@@ -16,7 +14,7 @@ public class Board {
     private int[][] board = new int[rows][columns];
 
     private final int i = rows - 1;
-    private int[] indices = new int[] {i, i, i, i, i, i, i}; // tracks next available row for column entries
+    private int[] rowTracker = new int[] {i, i, i, i, i, i, i}; // tracks next available row for column entries
 
     // Business methods
     public static Board newInstance() {
@@ -24,20 +22,20 @@ public class Board {
     }
 
     public boolean validMove(int column) {
-        return (column < indices.length && indices[column] >= 0);
+        return (column < rowTracker.length && rowTracker[column] >= 0);
     }
 
     public void occupySlot(Player player, int column) {
-        board[indices[column]][column] = player.getID();
+        board[rowTracker[column]][column] = player.getID();
 
-        indices[column]--;
+        rowTracker[column]--;
     }
 
     public boolean winnerDetected(Player player, int column) {
         boolean result = false;
 
         for (int i = 0; i < 7; i++) {
-            if (!result) result = BOARDSCANNER.checkRow(i, 0, player, this.board, indices[column], column);
+            if (!result) result = BOARDSCANNER.checkRow(i, 0, player, this.board, rowTracker[column], column);
             else break;
         }
         return result;
@@ -45,7 +43,7 @@ public class Board {
 
     public void clear() {
         board = new int[rows][columns];
-        indices = new int[] {i, i, i, i, i, i, i};
+        rowTracker = new int[] {i, i, i, i, i, i, i};
     }
 
     public int getColumns() {
