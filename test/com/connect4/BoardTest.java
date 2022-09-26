afeebecb6ed8b969f6344ea25dde03f877f26fee
Orlando <oredwardsjr@gmail.com>
2022-09-26 01:06:41 -0700

    Added testing packages and changed Board from singleton

---
+++ test/com/connect4/BoardTest.java
@@ -0,0 +1,12 @@
+package com.connect4;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
---
+++ test/com/connect4/CompLogicTest.java
@@ -0,0 +1,12 @@
+package com.connect4;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CompLogicTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
