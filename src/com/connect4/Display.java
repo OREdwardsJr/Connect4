0497b13612a8d9b4c861d72144fd48a40cc4f196
Coel316 <coel316@yahoo.com>
2022-09-27 09:13:17 -0700

    Updated Display to communicate with other packages.

--- src/com/connect4/Display.java
+++ src/com/connect4/Display.java
@@ -1,6 +1,6 @@
 package com.connect4;
 
-class Display {
+public class Display {
 
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
